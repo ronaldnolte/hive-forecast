0904825c80db4d3139ad1f3144ecfd0daaf00574
Ronald Nolte <ron.nolte@gmail.com>
2025-12-01 14:07:00 -0700

    Version with logos and titles corrected

---
+++ android/app/src/main/java/com/hiveforecast/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hiveforecast.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hiveforecast.app',
+  appName: 'Hive Forecast',
+  webDir: 'out'
+};
+
+export default config;
