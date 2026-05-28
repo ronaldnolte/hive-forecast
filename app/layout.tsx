6ca63932482e79856d67e955f806a4d6623e7d07
Ronald Nolte <ron.nolte@gmail.com>
2026-05-28 15:34:35 -0600

    chore: update installed PWA title to BeekTools Forecast

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
-    title: "Forecast",
+    title: "BeekTools Forecast",
   },
   manifest: "/manifest.json",
   icons: {
