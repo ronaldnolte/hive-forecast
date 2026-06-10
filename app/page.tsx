292c3e79fb1f5f1570a8168654cad1a23f4ffc50
Ronald Nolte <ron.nolte@gmail.com>
2026-06-09 19:13:41 -0600

    Add version label v1.0.1 for deployment verification

--- app/page.tsx
+++ app/page.tsx
@@ -68,6 +68,8 @@ export default function Home() {
 
   return (
     <main className="min-h-screen">
+      <div className="fixed bottom-2 right-3 text-[10px] text-gray-300 select-none z-50">v1.0.1</div>
+
       {/* Logo in upper left - Only visible on input screen */}
       {view === 'input' && (
         <div className="absolute top-4 left-4 z-50">
