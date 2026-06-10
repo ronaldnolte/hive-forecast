976a582cb8da788991cbb0dd4e9c5f5dfea2a0f7
Ronald Nolte <ron.nolte@gmail.com>
2026-06-09 19:20:01 -0600

    Darken version label to gray-500 for visibility

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
 
   return (
     <main className="min-h-screen">
-      <div className="fixed bottom-2 right-3 text-[10px] text-gray-300 select-none z-50">v1.0.1</div>
+      <div className="fixed bottom-2 right-3 text-[10px] text-gray-500 select-none z-50">v1.0.1</div>
 
       {/* Logo in upper left - Only visible on input screen */}
       {view === 'input' && (
